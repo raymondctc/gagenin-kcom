812e7b55ae05c0b9be48688651401c50c99f966d
Raymond Chan <raymond@9gag.com>
2018-06-19 18:24:06 +0800

    Init

---
+++ app/src/main/java/mock/ninegag/raymond/com/mockninegag/MainActivity.kt
@@ -0,0 +1,12 @@
+package mock.ninegag.raymond.com.mockninegag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
